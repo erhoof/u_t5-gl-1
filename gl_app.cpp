831c281c3d31ac5f01df2b08b895df6cdf456980
Pavel Bibichenko <erhoof@outlook.com>
2020-09-26 11:45:06 +0300

    Init OpenGL Extensions

--- gl_app.cpp
+++ gl_app.cpp
@@ -3,10 +3,42 @@
 
 void InitScene() // scene init
 {
+	glClearColor(0.25f, 1.0f, 0.25f, 0.0f);
 
+	// TODO: other init stuff
 }
 
 void InitOpenGLExt(HWND hWnd) // OpenGL extensions init
 {
+	hDC = GetDC(hWnd);
 
+	PIXELFORMATDESCRIPTOR pfd = { 0 }; // Pixel Format description
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR); // Structure size
+	pfd.dwFlags =
+		PFD_DRAW_TO_WINDOW |
+		PFD_SUPPORT_OPENGL | // OpenGL support
+		PFD_DOUBLEBUFFER; // Double Buffer support
+	pfd.iLayerType = PFD_MAIN_PLANE; // Layer type - Main / Overlay / Underlay
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cDepthBits = 24;
+	pfd.cColorBits = 24; // R + G + B (8+8+8) - 8 bit space
+	pfd.cAlphaBits = 8;
+	pfd.nVersion = 1; // Data structure version. Only v1 exists =)
+
+	// Find closest available pfd
+	auto pixelFormat = ChoosePixelFormat(hDC, &pfd);
+	
+	// Set pixel foramt
+	SetPixelFormat(hDC, pixelFormat, &pfd);
+
+	// Get pfn of opengl for windows exts by fake hrc
+	auto hrcFake = wglCreateContext(hDC); // Create temp rendering context
+	wglMakeCurrent(hDC, hrcFake); // Set cur context
+	wglChoosePixelFormatARB = (PFNWGLCHOOSEPIXELFORMATARBPROC)
+		wglGetProcAddress("wglChoosePixelFormatARB");
+	wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)
+		wglGetProcAddress("wglCreateContextAttribsARB");
+
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(hrcFake);
 }
--- gl_app.h
+++ gl_app.h
@@ -2,5 +2,19 @@
 
 #include <Windows.h>
 
+#include <GL/gl.h>
+#include <KHR/khrplatform.h> // EGL Platform : ogl <==> windows
+#include <GL/glext.h> // OpenGL 1.2 +
+#include <GL/glcorearb.h> // OpenGL core profile / ARB Extensions
+#include <GL/wglext.h> // Windows OpenGL Extension
+
+#define UPDATE_TIMER_ID 200 // for timer later
+
+// Global Variables
+HDC hDC = nullptr; // Device Context for the client area of a smt
+HGLRC hRC = nullptr; // Rendering Context
+PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr; // Pixel Format: opts of FB
+PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr; // Context Attribs
+
 void InitScene(); // scene init
 void InitOpenGLExt(HWND hWnd); // OpenGL extensions init
